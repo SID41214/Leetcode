b3324dda18bf12af2cf594b85ce2c0678e47ed69
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-03-29 15:10:57 +0530

    updated

---
+++ Edabit/Python/1.py
@@ -0,0 +1,5 @@
+#  Converting an Integer into Decimals
+import decimal
+a =int(input('enter integer to convert ='))
+print(decimal.Decimal(a))
+print(type(decimal.Decimal))
---
+++ Edabit/Python/2.py
@@ -0,0 +1,11 @@
+n=int(input("enter number to check = "))
+if n%2 == 0:
+    print("divisible by 2")
+elif n%3== 0:
+    print("divisible by3")
+else:
+    print("neither")
+    
+print("done")
+    
+    
