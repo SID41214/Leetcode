8aceb0d39e05e72892ec7853b2b44648f8e6a42d
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-04-03 21:39:51 +0530

    updated

--- Edabit/Python/Print.py
+++ Edabit/Python/Print.py
@@ -2,9 +2,9 @@
 
 # Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
-previous_num = 0
+# previous_num = 0
 
-for i in range(10):
-    x =previous_num + i
-    print("current number",i, "previous number",previous_num, "Sum:", x)
-    previous_num =i
+# for i in range(10):
+#     x =previous_num + i
+#     print("current number",i, "previous number",previous_num, "Sum:", x)
+#     previous_num =i
