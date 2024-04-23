f07a9d0fab857691e55957a2deaae321d9e25c2c
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-04-23 09:23:51 +0530

    updated

--- Python/reverse.py
+++ Python/reverse.py
@@ -22,20 +22,20 @@
 # Output: 21
 
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        is_negative = False
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         is_negative = False
 
-        if x < 0:
-            is_negative = True
-            x *= -1
+#         if x < 0:
+#             is_negative = True
+#             x *= -1
         
-        res = 0
-        while x > 0:
-            res = (res * 10) + (x % 10)
-            x //= 10
+#         res = 0
+#         while x > 0:
+#             res = (res * 10) + (x % 10)
+#             x //= 10
         
-        if res > 2 ** 31 - 1:
-            return 0
+#         if res > 2 ** 31 - 1:
+#             return 0
         
-        return res * -1 if is_negative else res
+#         return res * -1 if is_negative else res
